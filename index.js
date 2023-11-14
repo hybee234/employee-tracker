43b2beb495368ac56660e7ac6bcc4b0ab00042a1
Hybee <hybee234@outlook.com>
2023-11-14 23:21:14 +1100

    add Role almost finished

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const inquirer = require('inquirer');
 // const queries = require('./lib/server.js')
 const mainMenu = require ('./lib/mainMenu.js')    
 
+
+//-------------//
+//- Variables -// 
+//-------------//
+
+let arrayDept;       // array of departments available (used in creating new role)
+let roleDeptValue;   // new Role department value (used in creating new role)
+let addDeptCalledByAddRole = 0; // indicator to determine if addDepartment function was called by addRole
+
 //--------------------------//
 //- Connection to Database -// 
 //--------------------------//
 const db = require('./lib/connection.js')
-
-
 
 //---------------------------//
 //- Prompts to the end user -//
@@ -38,25 +45,11 @@ function launch() {
             //- Add role -//
                 case "addRole": addRole()
                 break;
-            //- Add employee -//
-                // id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                // first_name VARCHAR(30) NOT NULL,
-                // last_name VARCHAR(30) NOT NULL,
-                // role_id INT, - which role
-                // manager_id - which manager
-                case "addEmployee":
-                    console.log("  **addEmployee**")  
-                    return launch()
+            //- Add employee -//                
+                case "addEmployee": addEmployee()                
                 break;
             //- Update employee -//
-                // id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                // first_name VARCHAR(30) NOT NULL,
-                // last_name VARCHAR(30) NOT NULL,
-                // role_id INT, - which role
-                // manager_id - which manager
-                case "updateEmployee":
-                    console.log("  **updateEmployee**")  
-                    return launch()
+                case "updateEmployee": updateEmployee()
                 break;
             //- Quit -//
                 case "quit": 
@@ -70,7 +63,6 @@ function launch() {
             console.log(err)
         });
 }
-
 
 //-------------//
 //- Functions -//
@@ -132,25 +124,33 @@ const viewEmployees = () => {
 //---------------------------------//
 
 const addDepartment = async () => {
-    console.log(`\x1b[33m   **Add New Department**\x1b[0m`);
-    inquirer
-        .prompt ([
+    try {
+        console.log(`\x1b[33m   **Add New Department**\x1b[0m`);
+        const answers = await inquirer.prompt ([
             {
                 type: 'input',
                 name: 'newDepartment',
-                message: 'Please enter the new Department name'
+                message: 'Please enter the new Department name:'
             }
-        ]).then ((answers) => {
-            db.query('INSERT INTO department (name) VALUES (?)', answers.newDepartment, async function (err, result) {
-                try{                                    
-                    const show = await console.log(`\x1b[33m   ** ${answers.newDepartment} added successfully**\x1b[0m`)
-                    const startagain = await launch();
-                }
-                catch (err) {
-                    console.log(err)
-                }
-            })
-        }) 
+        ])
+        db.query('INSERT INTO department (name) VALUES (?)', answers.newDepartment, async (err, result) => {
+            try{                                    
+                const show = await console.log(`\x1b[33m\n   ** New departemnt "${answers.newDepartment}" added successfully**\x1b[0m`)
+                roleDeptValue = await result.insertId
+                const log = await console.log (roleDeptValue)
+                if (addDeptCalledByAddRole === 1) {         // Check if addDepartment was called by Add Role - if yes then change addDpetCalledByAddRole to zero and return to add Role 
+                    addDeptCalledByAddRole = 0;
+                    return (roleDeptValue)}
+                else {const startagain = await launch();}   // Else, go back to main menu (call launch())             
+            }
+            catch (err) {
+                console.log(err)
+            }
+        })
+    }catch (err) {
+        console.log(err)
+    };
+    console.log ("addDepartment finished executing")
 };
 
 //---------------------------//
@@ -163,13 +163,12 @@ const addDepartment = async () => {
 
 const addRole = async () => {
     console.log(`\x1b[33m   **Add new Role**\x1b[0m`);
-    let arrayDept;
 
     // Create department constant
     try {
-        const response = await db.promise().query(`SELECT id as value, name FROM department;`)   // Pull fresh extract of department
+        const response = await db.promise().query(`SELECT id as value, name FROM department;`)                    // Pull fresh extract of department
         
-        arrayDept = response[0]                                                                 //set ArrayDept to equal reponse index value zero
+        arrayDept = response[0]                                                                                   // set ArrayDept to equal reponse index value zero
         arrayDept.unshift({value: -1, name: ' ↻ Back to main Menu'}, {value: 0, name: 'Create Department'})       // return to main menu and create department options
         
         console.log("Array Dept")
@@ -183,79 +182,71 @@ const addRole = async () => {
                 choices: arrayDept
             },
         ])
-
-        //Turn this if statement into a proper async/await so that the next process waits for it
-
-        let roleDeptValue;
-        
-        
-        if (await answer.roleDepartment === 0) {              // if equal to 'Create Department"
+                
+        if (answer.roleDepartment === 0) {              // if equal to 'Create Department"
+            console.log ("roleDepartment === 0, calling add Department")
+            addDeptCalledByAddRole = 1                  // Set "addDeptCalledbyAddRole = 1" to indicate YES
+            console.log (`addDeptCalledByAddRole = ${addDeptCalledByAddRole}`)
             roleDeptValue = await addDepartment();     // call Add department function
-        } else if (await answer.roleDepartment === -1) {
-            launch();
+        } else if (answer.roleDepartment === -1) {      // Back to main menu
+            console.log ("roleDepartment === -1, Back to main menu")
+            await launch();
             return;
         } else {
-            roleDeptValue = await answer.roleDepartment;     // use value provided           
+            console.log ("roleDepartment is good value, existing department")
+            roleDeptValue = answer.roleDepartment;     // use department value provided           
         }
-
-        // const a = await console.log("roleDeptValue:")
-        // const b = await console.log(roleDeptValue)
-
-        // asks the rest of the questions to gather what's needed for the Role
-        // 
-
-        // launch();
-
+        
+        // Ask for the rest of the values for the role//
+        const answers = await inquirer.prompt ([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the Title of the Role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the Salary of the Role?'
+            }
+        ])
+        // Insert the record into the role table//
+        await db.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [answers.title, answers.salary, roleDeptValue]);
+        console.log(`\x1b[33m\n   **New Role "${answers.title}" added successfully**\x1b[0m`)   
+        const startagain = await launch();    
     } catch (err) {
         console.log(err);
         
     }
 };
-    
+
+//-------------------------------//
+//- Function - Add new Employee -//
+//-------------------------------//
+// id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+// first_name VARCHAR(30) NOT NULL,
+// last_name VARCHAR(30) NOT NULL,
+// role_id INT, - which role
+// manager_id - which manager
 
 
-    // inquirer
-    //     .prompt ([
-    //         {
-    //             type: 'input',
-    //             name: 'title',
-    //             message: 'Title of new Role'
-    //         },
-    //         {
-    //             type: 'input',
-    //             name: 'salary',
-    //             message: 'Salary of new Role'
-    //         },
-    //         {
-    //             type: 'list',
-    //             name: 'department',
-    //             message: 'Which department does this role belong in?',
-    //             // choices: db.query(`SELECT id AS value, name FROM department;`, async function (err, results) {
-    //             //     try{
-    //             //         const query = await console.log(`\x1b[33m   **${answers.mainmenu}**\x1b[0m`);
-    //             //         const show = await console.table(results)
-                //         const startagain = await launch();
-                //     }
-                //     catch (err) {
-                //     console.log(err)
-                //     }
-//                 // })
-//                 // choices: [1, 2, 3]
-//                 choices: deptChoice
-//             }
-//         ]).then ((answers) => {
-//             // db.query('INSERT INTO department (name) VALUES (?)', answers.newDepartment, async function (err, result) {
-//             //     try{                                    
-//             //         const show = await console.log(`\x1b[33m   ** ${answers.newDepartment} added successfully**\x1b[0m`)
-//             //         const startagain = await launch();
-//             //     }
-//             //     catch (err) {
-//             //         console.log(err)
-//                 // }
-//             // })
-//             console.log(answers)
-//         })                       
-// };
+
+
+
+const addEmployee = async () => {}
+
+
+
+
+//-------------------------------//
+//- Function - Update Employee -//
+//-------------------------------//
+
+// id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+// first_name VARCHAR(30) NOT NULL,
+// last_name VARCHAR(30) NOT NULL,
+// role_id INT, - which role
+// manager_id - which manager
 
 
 //---------------------------------//
